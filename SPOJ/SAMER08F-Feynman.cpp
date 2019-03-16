94252cf57ac468f5087a3df296e72697c1a3e70c
ankitvashisht12 <ankitvashisht12@gmail.com>
2019-03-16 23:33:43 +0530

    SAMER08F-SPOJ

---
+++ SPOJ/SAMER08F-Feynman.cpp
@@ -0,0 +1,19 @@
+/*
+ * Author : Ankit Vashisht
+ * Problem :https://www.spoj.com/problems/SAMER08F/
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{   
+    int n;
+    while(true){
+        cin>>n;
+        if(n!=0)
+        cout<< (n*(n+1)*(2*n +1)/6)<<'\n';
+        else break;
+    }
+    return 0;
+}
