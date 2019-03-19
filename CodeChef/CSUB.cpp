d838916ccd276886daaeb67bc540890ca01b55f4
ankitvashisht12 <ankitvashisht12@gmail.com>
2019-03-19 20:28:34 +0530

    Codechef

---
+++ CodeChef/CSUB.cpp
@@ -0,0 +1,40 @@
+/*
+ * Author : Ankit Vashisht
+ * Problem : https://www.codechef.com/problems/CSUB
+ */
+
+#include <bits/stdc++.h>
+#define endl '\n'
+using namespace std;
+
+int main()
+{
+    int t; 
+    cin>>t; 
+    while(t--){
+        int len;
+        cin>>len;
+        string s;
+        cin>>s;
+        int c=0;
+        int n = s.length();
+        if(n == len){
+            for(int i = 0; i < n; i++)
+        {
+            if(s[i]  == '1'){
+                c++;
+                for(int j = i+1; j < n; j++)
+                {
+                    if(s[j] == '1') c++;
+                }
+                
+            } 
+        }
+         cout<<c<<endl;
+        }
+       
+        
+    }
+    
+    return 0;
+}
