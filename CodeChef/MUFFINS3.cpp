cd0340349aecbb37ed4cd44901a499183f9d9460
ankitvashisht12 <ankitvashisht12@gmail.com>
2019-03-20 17:43:31 +0530

    Codechef

---
+++ CodeChef/MUFFINS3.cpp
@@ -0,0 +1,23 @@
+/*
+ * Author : Ankit Vashisht
+ * Problem :
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define int long long
+const int N=1e5+5;
+
+int32_t main(){
+    IOS;
+    int t,n;
+    cin>>t;
+    while(t--){
+        cin>>n;
+        cout<< (n/2)+1<<endl;
+    }
+    
+    return 0;
+}
